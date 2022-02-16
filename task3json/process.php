2b9577788369f7a70a5002855e00e973fe456743
ranatabassum <98724386+ranatabassum@users.noreply.github.com>
2022-02-16 22:55:07 +0600

    Add files via upload

---
+++ task3json/process.php
@@ -0,0 +1,38 @@
+<?php
+   	
+$formdata = array(
+	      'firstName'=> $_POST["firstName"],
+	      'lastName'=> $_POST["lastName"],
+	      'age'=>$_POST["age"],
+	      'designation'=> $_POST["designation"]
+	      'preferredLanguage'=> $_POST["preferredLanguage"],
+	      'password'=>$_POST["password"],
+	      
+	   );
+       $existingdata = file_get_contents('data.json');
+       $tempJSONdata = json_decode($existingdata);
+       $tempJSONdata[] =$formdata;
+       //Convert updated array to JSON
+	   $jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
+	   
+	   //write json data into data.json file
+	   if(file_put_contents("data.json", $jsondata)) {
+	        echo "Data successfully saved <br>";
+	    }
+	   else 
+	        echo "no data saved";
+
+     $data = file_get_contents("data.json");
+	 $mydata = json_decode($data);
+
+    
+	 echo "my value: ".$mydata[1]->lastName;
+	 foreach($mydata as $myobject)
+	 {
+	 foreach($myobject as $key=>$value)
+	{
+		echo "your ".$key." is ".$value."<br>";
+	} 
+	}
+	
+?>
---
+++ task3json/task3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> Registration Form </h1>
+<form action="action.php">
+First Name <br>
+<input type = "text" name= "First Name" > <br>
+Last Name <br>
+<input type = "text" name= "Last Name" > <br>
+Age <br>
+<input type = "number"> <br>
+Designation <br>
+<input type = "radio" name="dsg">Junior Programmer
+<input type = "radio" name="dsg">Senior Programmer
+<input type = "radio" name="dsg">Project Lead <br>
+Preferred Language <br>
+<input type = "checkbox" id="plng">JAVA
+<input type = "checkbox" id="plng">PHP
+<input type = "checkbox" id="plng">C++ <br>
+Email <br>
+<input type = "text" name= "Email" > <br> 
+Password<br>
+<input type = "password" name= "Email" > <br>
+Please choose a file
+<input type="file" id="myFile" name="filename"> <br>
+<input type="submit" value="Submit">
+<input type="Reset" value = "Reset">
+
+</body>
+</html>
